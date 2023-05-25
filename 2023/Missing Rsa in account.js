302fef6071e67706c5030d73c9b5d287dfd8b233
Arjan <schoorl.arjan@outlook.com>
2023-05-25 15:02:59 +0200

    Update and rename missing rsa.js to Missing Rsa in account.js

--- 2023/Missing Rsa in account.js
+++ 2023/Missing Rsa in account.js
@@ -1,3 +1,15 @@
+// Copyright 2023. All Rights Reserved.
+//
+// Script by: Arjan Schoorl
+//
+// ABOUT THE SCRIPT
+// This Google Ads script scans through all active search campaigns
+// and their respective active ad groups to identify if any Responsive 
+// Search Ads (RSAs) are missing. It then logs the total number of 
+// active ad groups that are missing at least one RSA.
+//
+////////////////////////////////////////////////////////////////////
+
 function main() {
   var missingCount = 0;
 
